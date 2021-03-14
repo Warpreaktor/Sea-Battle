916a3bfec5a0c1c72300fe979acd54113d401154
Warpreaktor <warpreaktor@gmail.com>
2021-03-14 22:55:50 +0300

    Работаем над интеграцией GUI

--- src/com/core/SeaBattleGame.java
+++ src/com/core/SeaBattleGame.java
@@ -4,19 +4,55 @@ import front.Game;
 import front.Color;
 
 public class SeaBattleGame extends Game {
-
     private static int SIZE = 10;
     private static int totalShips = 0;
+    private static GameCell[][] gameField = new GameCell[SIZE][SIZE];
+
+    public static int getSIZE() {
+        return SIZE;
+    }
+
+    public static int getTotalShips() {
+        return totalShips;
+    }
+    public static void setTotalShips(int ships) {
+        totalShips = ships;
+    }
+
+    @Override
+    public void initialize() {
+        setScreenSize(SIZE, SIZE);
+        createGame();
+    }
+    private void createGame() {
+        for (int y = 0; y < SIZE; y++) {
+            for (int x = 0; x < SIZE; x++) {
+                //setCellValue(x,y,"");
+                gameField[y][x].setCellLabel('R');
+                boolean isMine = getRandomNumber(15) < 1;
+                if (isMine) {
+                    System.out.println("+минка");
+                    totalShips++;
+                }
+                gameField[y][x] = new GameCell();
+                setCellColor(x, y, Color.WHITESMOKE);
+            }
+        }
+        //countMineNeighbors();
+        //countFlags = countMinesOnField;
+    }
+
 
     public static void main(String[] args) {
+
         //Tests.mainTest();
-        Player player1 = new Player("Warper");
-        Player playerCPU = new Player("CPU");
-        playerCPU.setCPU(true);
-        player1.shipsOnTheField();
-        playerCPU.shipsOnTheField();
-        player1.brushTheMap();
-        battle(player1, playerCPU);
+        //Player player1 = new Player("Warper");
+        //Player playerCPU = new Player("CPU");
+        //playerCPU.setCPU(true);
+        //player1.shipsOnTheField();
+        //playerCPU.shipsOnTheField();
+        //player1.brushTheMap();
+        //battle(player1, playerCPU);
     }
 
     public static void battle(Player player1, Player player2) {
@@ -32,19 +68,6 @@ public class SeaBattleGame extends Game {
         }
     }
 
-    public static void setSIZE(int SIZE) {
-        SeaBattleGame.SIZE = SIZE;
-    }
 
-    public static int getSIZE() {
-        return SIZE;
-    }
 
-    public static void setTotalShips(int ships) {
-        totalShips = ships;
-    }
-
-    public static int getTotalShips() {
-        return totalShips;
-    }
 }
